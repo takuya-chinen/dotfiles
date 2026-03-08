9516be3036ebe8eec0945ae3ff277ef7a0f3dbff
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-03-08 15:02:46 +0900

    markview.nvimを追加
    markviewを起動時オフにして、任意のタイミングで設定できるようにした

---
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,13 @@
+-- For `plugins/markview.lua` users.
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        enable = false,
+      }
+    } 
+
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+};
